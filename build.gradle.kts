609bb34010eb433eb7032e72efd6ebe3fa20d43a
GhostTypes <106415648+GhostTypes@users.noreply.github.com>
2026-02-04 09:37:09 -0500

    Add modInclude helper for bundled deps

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,16 @@
+import org.gradle.api.artifacts.MinimalExternalModuleDependency
+import org.gradle.api.provider.Provider
+import org.gradle.kotlin.dsl.DependencyHandlerScope
+
 plugins {
     alias(libs.plugins.fabric.loom)
+}
+
+fun DependencyHandlerScope.modInclude(
+    dependencyProvider: Provider<out MinimalExternalModuleDependency>,
+) {
+    modImplementation(dependencyProvider)
+    include(dependencyProvider)
 }
 
 base {
@@ -31,11 +42,11 @@ dependencies {
     compileOnly(libs.orbit)
 
     // NanoHTTPD for HTTP server and WebSocket support
-    include(modImplementation(libs.nanohttpd.core.get())!!)
-    include(modImplementation(libs.nanohttpd.websocket.get())!!)
+    modInclude(libs.nanohttpd.core)
+    modInclude(libs.nanohttpd.websocket)
 
     // JSON serialization for WebSocket messages
-    include(modImplementation(libs.gson.get())!!)
+    modInclude(libs.gson)
 
     // Testing
     testImplementation(libs.junit.api)
